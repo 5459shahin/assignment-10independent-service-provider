eabd9294b9b2bda8a960df8acb1d0ebb0b726bbf
545n shahin <mohammadshahinkhan1992@gmail.com>
2022-04-19 22:32:23 +0800

    added email login

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -1,22 +1,22 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import {getAuth} from 'firebase/auth';
-import App from "./App";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey:process.evn.REACT_APP_apiKey,
-  authDomain:process.evn.REACT_APP_authDomain,
-  projectId:process.evn.REACT_APP_projectId,
-  storageBucket:process.evn.REACT_APP_storageBucket,
-  messagingSenderId:process.evn.REACT_APP_messagingSenderId,
-  appId:process.evn.REACT_APP_appId,
+  apiKey:process.env.REACT_APP_apiKey,
+  authDomain:process.env.REACT_APP_authDomain,
+  projectId:process.env.REACT_APP_projectId,
+  storageBucket:process.env.REACT_APP_storageBucket,
+  messagingSenderId:process.env.REACT_APP_messagingSenderId,
+  appId:process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-const auth = getAuth(App);
+const auth = getAuth(app);
 export default auth;
